d81cb0718b8eb3396793e2c2868948e7a756305f
Tim-ga <tim.gga@gmail.com>
2022-01-21 20:41:44 +0300

    Added tests

---
+++ src/FactorialFibonacci/FibonacciTest.java
@@ -0,0 +1,20 @@
+package FactorialFibonacci;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FibonacciTest {
+
+    @Test
+    public void fibonacciCycleWhile() {
+    }
+
+    @Test
+    public void fibonacciCycleDoWhile() {
+    }
+
+    @Test
+    public void fibonacciCycleFor() {
+    }
+}
